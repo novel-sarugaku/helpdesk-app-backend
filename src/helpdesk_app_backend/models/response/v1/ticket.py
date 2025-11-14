49f35366e3c363e9eb5cec26e2f27602cca5393a
miho.sarugaku <miho.sarugaku@novel.jpn.com>
2025-11-14 12:14:58 +0900

    feat: チケット更新（サポート担当者設定）APIの追加

--- src/helpdesk_app_backend/models/response/v1/ticket.py
+++ src/helpdesk_app_backend/models/response/v1/ticket.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from helpdesk_app_backend.models.enum.ticket import TicketStatusType
 
@@ -34,6 +34,9 @@ class GetTicketDetailResponse(BaseModel):
     description: str
     supporter: str | None
     created_at: datetime
+    is_own_ticket: bool = Field(
+        default=False
+    )  # 担当が自分であるかどうかのフラグであるが、サポーターが存在しない場合も false となる（FE側で supporter が null の時点で、is_own_ticket の値は気にしないためOK）
     ticket_histories: list[GetTicketHistoryResponseItem]
 
 
@@ -46,3 +49,10 @@ class CreateTicketResponse(BaseModel):
     description: str
     staff: int
     created_at: datetime
+
+
+# チケット更新（PUT）
+class UpdateTicketResponse(BaseModel):
+    id: int
+    status: TicketStatusType
+    supporter: str
